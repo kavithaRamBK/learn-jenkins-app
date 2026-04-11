2921a197ec4b0ca0d0457efacec6824f50631802
kavithaRamBK <kavitharamcs@gmail.com>
2026-04-11 14:59:13 +0530

    up date git poll

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy and devops ND
+          Learn Jenkins on Udemy and devops ND Runnin Git Polling
         </a>
       </header>
       <p>
